b0736c15cbc95f7d473ac2a4504e886f77e810e8
Kondameedi Satwika <kondameedisatwika09@gmail.com>
2026-06-14 17:57:51 +0530

    Added login automation test

---
+++ selenium-project/src/test/java/com/test/LoginTest.java
@@ -0,0 +1,42 @@
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+    @Test
+    public void loginTest() throws Exception {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("file:///C:/Users/konda/OneDrive/Desktop/sample/sample/login.html");
+
+        driver.findElement(By.id("username"))
+              .sendKeys("admin");
+
+        driver.findElement(By.id("password"))
+              .sendKeys("1234");
+
+        driver.findElement(By.tagName("button"))
+              .click();
+
+        Thread.sleep(3000);
+
+        String actualMessage =
+                driver.findElement(By.id("message"))
+                      .getText();
+
+        Assert.assertEquals(actualMessage,
+                "Login successful!");
+
+        System.out.println("Test Passed");
+
+        //driver.quit();
+    }
+}
